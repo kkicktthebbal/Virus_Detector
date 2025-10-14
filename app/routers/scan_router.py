92f07845fcf266ea00d8d0ba4832d6ee2a2bdf8f
seungmingyu <seung7549@naver.com>
2025-10-15 03:59:21 +0900

    index페이지

--- app/routers/scan_router.py
+++ app/routers/scan_router.py
@@ -7,6 +7,7 @@ from fastapi import APIRouter, UploadFile, File
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from app.LLM.gemini import generate
+from fastapi.responses import HTMLResponse
 
 router = APIRouter(
     prefix = "/scan"
@@ -16,6 +17,10 @@ templates = Jinja2Templates(directory="templates")
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 UPLOAD_DIR = os.path.join(BASE_DIR, "static", "file")
+@router.get("/office-hwp", response_class=HTMLResponse)
+def index_page(request: Request):
+        return templates.TemplateResponse("ms.html", {"request": request})
+
 
 @router.post("/ms")
 async def scan_ms(requset: Request, file: UploadFile = File(...)):
