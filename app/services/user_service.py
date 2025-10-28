76eb4e41e955ae537103083fa9535fb0ecd2701a
seungmingyu <seung7549@naver.com>
2025-10-28 14:58:00 +0900

    로그인 회원가입

---
+++ app/services/user_service.py
@@ -0,0 +1,30 @@
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from app.schemas.user_schema import LocalUserCreate
+from app.models.user import User
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_local_user(db : Session, schema : LocalUserCreate):
+    hashed_password = hash_password(schema.password)
+    new_user = User(user_id = schema.user_id,
+                    password = hashed_password,
+                    name = schema.name,
+                    email = schema.email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+def local_login(db: Session, user_id: str, password: str):
+
+    user = db.query(User).filter(User.user_id == user_id).first()
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
